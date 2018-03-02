bf60b024d6a3feb0ecd1dfdcecdf7e3923d0f2d7
Tobe O <thosakwe@gmail.com>
2018-03-01 20:10:19 -0500

    Finish MockProcessManager tests

--- test/mock_test.dart
+++ test/mock_test.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
 import 'package:process_plus/process_plus.dart';
 import 'package:test/test.dart';
+import 'completed_process_test.dart';
 
 void main() {
-  var manager = new MockProcessManager();
+  var r = new ProcessResult(0, 0, hello, world);
+  MockProcessManager manager;
+
+  setUp(() async {
+    manager = new MockProcessManager();
+  });
 
   test('canRun', () {
     manager..enqueueCanRun(true)..enqueueCanRun(false);
@@ -16,7 +23,20 @@ void main() {
     expect(manager.killPid(0), false);
   });
 
-  test('queue process', () async {});
+  test('queue process', () async {
+    manager.enqeueProcess(new CompletedProcess(r));
+    var p = await manager.start(['foo']);
+    expect(await p.pid, r.pid);
+    expect(await p.exitCode, r.exitCode);
+    expect(await p.stdout.first, hello);
+    expect(await p.stderr.first, world);
+  });
 
-  group('queue process result', () {});
+  group('queue process result', () {
+    setUp(() async {
+      manager..enqeue(r)..enqeue(r);
+      expect(manager.runSync(['foo']), r);
+      expect(await manager.run(['foo']), r);
+    });
+  });
 }
